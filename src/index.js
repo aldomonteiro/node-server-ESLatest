1447e3f5d3c927617596fe7ce591bbe4db665562
Aldo Monteiro <aldomonteiro@gmail.com>
2019-06-26 21:06:36 -0300

    all features

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-const cars = { BMW: 3, Tesla: 2, Toyota: 1 };
+import express from 'express';
 
-/** 
- * 1. Object.values()
- * Object.values() is a new function that’s similar to Object.keys() but returns 
- * all the values of the Object’s own properties excluding any value(s) in the prototypical chain.
-*/
-const values = Object.values(cars);
-console.log(values);
+const app = express();
+const state = 'bitcointrade';
 
-/**
- * 2. Object.entries()
- *  Object.entries() is related to Object.keys , 
- * but instead of returning just keys, it returns both keys and values in the array fashion. 
- * This makes it very simple to do things like using objects in loops or converting objects into Maps.
- */
-for (let [key, value] of Object.entries(cars)) {
-  console.log(`key:${key}, value:${value}`);
-}
+app.get('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).json('Hello world!');
+});
 
-/**
- * 3. String padding
- * Two instance methods were added to String — String.prototype.padStart and String.prototype.padEnd 
- * — that allow appending/prepending either an empty string or some other string to the start or 
- * the end of the original string.
- */
+app.get('/abc123_', (req, res) => {
+  console.log('get abc123_:', req);
+  res.status(200);
+});
 
-/**
- * 3.1 padStart example:
- * In the below example, we have a list of numbers of varying lengths. We want to prepend “0” so that 
- * all the items have the same length of 3 digits for display purposes. 
- * We can use padStart(3, '0') to easily achieve this.
- */
-const numbers = [1, 3, 4, 10, 20, 51, 90, 100, 101, 200]
-numbers.map(unformatted => console.log(unformatted.toString().padStart(3, '0')))
+app.get('/web/abc123_v1', (req, res) => {
+  console.log('get abc123_v1:', req);
+  res.status(200);
+});
+
+app.get('/web/abc123_v1', (req, res) => {
+  console.log('v1 post:', req);
+  res.status(200);
+});
+
+
+app.post('/', (req, res) => {
+  console.log('post:', req);
+  res.status(200);
+});
+
+const port = 4000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
